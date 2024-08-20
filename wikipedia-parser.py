958cca3a18cb759c6b55304c46ca17c0e55c87a4
Panda <panda@PandaOS>
2024-08-20 21:11:56 +0200

    Added article cleaning, status messages and added the functionallity to put the parsed data in a sqlite3 database

--- wikipedia-parser.py
+++ wikipedia-parser.py
@@ -1,17 +1,31 @@
 from xml.sax import ContentHandler, parse
+
 from logging import getLogger
+
 from bz2 import BZ2File
+from sqlite3 import SQLITE_OK
+from sqlite3 import connect as sqlite3_connect
+
 from argparse import ArgumentParser
-from multiprocessing import Manager, Process
+
+from multiprocessing import Manager, Process, Value
 from threading import Thread
+
 from os import path
 from sys import exit
+from time import sleep
+
+from re import sub, DOTALL
+
 
 logger = getLogger("wikipedia-parser")
 
 articleQueue = None
 writeQueue = None
 allDataRead = None
+
+conn = None
+writtenCount = Value("i", 0)
 
 class WikiParser(ContentHandler):
     def __init__(self, namespace_filter, callback):
@@ -69,13 +83,44 @@ class WikiParser(ContentHandler):
 def processArticles():
     while not (allDataRead and articleQueue.empty()):
         title,text = articleQueue.get()
+        
+        # https://github.com/awslabs/fever/blob/master/fever-annotations-platform/src/dataset/reader/cleaning.py
+        cleanedText = text
 
-        writeQueue.put(title)
+        # HTML Comments
+        cleanedText = sub(r'(<!--.*?-->)', "", cleanedText, flags=DOTALL)
+        # Refs
+        cleanedText = sub(r'<ref( name ?= ?\"?(.*?)\"?)?((>(.*?)<\/ref>)|(\ ?\/>))', r'', cleanedText, flags=DOTALL)
+        cleanedText = sub(r'\[\[(.*?)\]\]', r'', cleanedText, flags=DOTALL)
+        # Files
+        cleanedText = sub(r'(?i)\{\{IPA(\-[^\|\{\}]+)*?\|([^\|\{\}]+)(\|[^\{\}]+)*?\}\}', lambda m: " -LSB- "+ m.group(2)+" -RSB- ", cleanedText)
+        cleanedText = sub(r'(?i)\{\{Convert\|(.*?)\|(.*?)(\|.*?)\}\}', lambda m: m.group(1)+m.group(2), cleanedText)
+        cleanedText = sub(r'(?i)\[\[wikt\:(.*?)\|.*?\]\]', lambda m: m.group(1), cleanedText)
+
+        writeQueue.put((title, cleanedText))
 
 def processWriting(outFile):
+    conn = sqlite3_connect(outFile)
+    cursor = conn.cursor()
+
+    cursor.execute("CREATE TABLE IF NOT EXISTS pages (title text, content text)")
+
     while not (allDataRead and writeQueue.empty()):
-        out = writeQueue.get()
-        outFile.write(out + "\n")
+        title,text = writeQueue.get()
+        cursor.execute("INSERT OR IGNORE INTO pages VALUES (?, ?)", (title, text))
+        conn.commit()
+        with writtenCount.get_lock():
+            writtenCount.value += 1
+
+# https://jamesthorne.com/blog/processing-wikipedia-in-a-couple-of-hours/
+def display():
+    while True:
+        print("Queue sizes: articleQueue={0} writeQueue={1} Read: {2} Written: {3}".format(
+            articleQueue.qsize(), 
+            writeQueue.qsize(), 
+            parser.pages_processed,
+            writtenCount.value))
+        sleep(1)
 
 if __name__ == "__main__":
     allDataRead = False
@@ -88,23 +133,27 @@ if __name__ == "__main__":
     arguments = argumentParser.parse_args()
 
     manager = Manager()
-    articleQueue = manager.Queue(maxsize=2048)
-    writeQueue = manager.Queue(maxsize=2048)
+    articleQueue = manager.Queue()
+    writeQueue = manager.Queue()
 
     wikiData = BZ2File(arguments.data_in)
-    outFile = open(path.join(arguments.data_out), "a+") 
-    
-    processes = []
-    for i in range(arguments.processes):
-        process = Process(target=processArticles)
-        process.start()
-        processes.append(process)
-
-    writeThread = Thread(target=processWriting, args=(outFile,))
-    writeThread.start()
+    outFile = path.join(arguments.data_out) 
     
     try:
         parser = WikiParser(lambda ns: ns == 0, articleQueue.put)
+
+        statusThread = Thread(target=display, args=())
+        statusThread.start() 
+
+        processes = []
+        for i in range(arguments.processes):
+            process = Process(target=processArticles)
+            process.start()
+            processes.append(process)
+        
+        writeThread = Thread(target=processWriting, args=(outFile,))
+        writeThread.start()
+
         parse(wikiData, parser)
 
         allDataRead = True
