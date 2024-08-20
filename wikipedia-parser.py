f96a3e7f7fbb3f6fec32e2b88d83fb81f019dd21
Panda <panda@PandaOS>
2024-08-20 19:23:19 +0200

    Added multithreading, article processing and queues

--- wikipedia-parser.py
+++ wikipedia-parser.py
@@ -1,8 +1,17 @@
 from xml.sax import ContentHandler, parse
 from logging import getLogger
 from bz2 import BZ2File
+from argparse import ArgumentParser
+from multiprocessing import Manager, Process
+from threading import Thread
+from os import path
+from sys import exit
 
 logger = getLogger("wikipedia-parser")
+
+articleQueue = None
+writeQueue = None
+allDataRead = None
 
 class WikiParser(ContentHandler):
     def __init__(self, namespace_filter, callback):
@@ -57,14 +66,67 @@ class WikiParser(ContentHandler):
         elif self.read_stack[-1] == "text":
             self.current_text += content
 
-def printit(inputs):
-    title,text = inputs
-    print(title)
-    #print(text)
+def processArticles():
+    while not (allDataRead and articleQueue.empty()):
+        title,text = articleQueue.get()
+
+        writeQueue.put(title)
+
+def processWriting(outFile):
+    while not (allDataRead and writeQueue.empty()):
+        out = writeQueue.get()
+        outFile.write(out + "\n")
 
 if __name__ == "__main__":
-    wiki = BZ2File("data.xml.bz2")
-    parser = WikiParser(lambda ns: ns == 0, printit)
+    allDataRead = False
 
-    parse(wiki, parser)
+    argumentParser = ArgumentParser()
+    argumentParser.add_argument("--processes", type=int, default=15)
+    argumentParser.add_argument("data_in", help="Example: data.xml.bz2")
+    argumentParser.add_argument("data_out", help="Example: database.db")
+    
+    arguments = argumentParser.parse_args()
+
+    manager = Manager()
+    articleQueue = manager.Queue(maxsize=2048)
+    writeQueue = manager.Queue(maxsize=2048)
+
+    wikiData = BZ2File(arguments.data_in)
+    outFile = open(path.join(arguments.data_out), "a+") 
+    
+    processes = []
+    for i in range(arguments.processes):
+        process = Process(target=processArticles)
+        process.start()
+        processes.append(process)
+
+    writeThread = Thread(target=processWriting, args=(outFile,))
+    writeThread.start()
+    
+    try:
+        parser = WikiParser(lambda ns: ns == 0, articleQueue.put)
+        parse(wikiData, parser)
+
+        allDataRead = True
+    except KeyboardInterrupt:
+        print("KeyboardInterrupt detected. Terminating...")
+        
+        outFile.close()
+        print("Closed outFile")
+        
+        while not articleQueue.empty():
+            articleQueue.get()
+        print("Cleared articleQueue")
+        for process in processes:
+            process.terminate()
+            process.join()
+        print("Terminated processes")
+        
+        allDataRead = True
+        while not writeQueue.empty():
+            writeQueue.get()
+        print("Cleared writeQueue")
+
+        print("Exiting...")
+        exit(1)
 
